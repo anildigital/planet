ec297826c7766e59d0388333cef0ba69eab49600
Anil Wadghule <anildigital@gmail.com>
2008-05-31 13:53:14 +0530

    Added order pubdate descending

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @feeds = Feed.paginate(:all, :per_page => 5, :page => params[:page])
+    @feeds = Feed.paginate(:all, :per_page => 5, :page => params[:page], :order => "pubDate DESC")
   end
 end
