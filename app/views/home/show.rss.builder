32415bc3aacfbd44257952dc4d80b9c9a3406e90
Anil Wadghule <anildigital@gmail.com>
2008-10-21 00:18:36 +0530

    Changed word Planet Rails to Planet Ruby on Rails Minor changes and improvements

--- app/views/home/show.rss.builder
+++ app/views/home/show.rss.builder
@@ -2,7 +2,7 @@ xml.instruct! :xml, :version => "1.0"
 xml.rss :version => "2.0" do
   xml.channel do
 
-    xml.title "Planet Rails Feed"
+    xml.title "Planet Ruby on Rails Feed"
     xml.description "Combines top rails blogs feeds into one."
     xml.link "http://feeds.feedburner.com/PlanetRails"
 
--- config/database.rb
+++ config/database.rb
@@ -1,37 +1,5 @@
-require 'erb'
-
-before "deploy:setup", :db
-after "deploy:update_code", "db:symlink"
-
-namespace :db do
-  desc "Create database yaml in shared path"
-  task :default do
-    db_config = ERB.new <<-EOF
-              base: &base
-                adapter: mysql
-                socket: /var/run/mysqld/mysqld.sock
-                username: #{user}
-                      password: #{password}
-
-                          development:
-                            database: #{application}_dev
-                            <<: *base
-
-                          test:
-                            database: #{application}_test
-                            <<: *base
-
-                          production:
-                            database: #{application}_production
-                            <<: *base
-                          EOF
-
-                      run "mkdir -p #{shared_path}/config"
-                      put db_config.result, "#{shared_path}/config/database.yml"
-  end
-
-  desc "Make symlink for database yaml"
-  task :symlink do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  end
+task :after_symlink do
+    %w{ database.yml}.each do |c|
+      run "ln -nsf #{shared_path}/system/config/#{c} #{current_path}/config/#{c}"
+    end
 end
