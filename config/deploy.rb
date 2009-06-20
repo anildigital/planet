5da50d8be37db6adc3c1623a485866c9c435f234
Anil <Anil@anils-macbook.local>
2009-06-20 17:03:03 +0530

    Changes for passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repository,  "git://github.com/anildigital/planet.git"
 
 set :deploy_via, :remote_cache 
 set :deploy_to, "/home/anil/public_html/#{application}"
-set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
+#set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 
 set :port, 22
 
@@ -11,18 +11,18 @@ set :scm, :git
 
 ssh_options[:paranoid] = false
 
-set :user, 'anil'
+set :user, :anil
 set :runner, user
-set :use_sudo, 'false'
+set :use_sudo, false
 
 role :app, application
 role :web, application
 role :db,  application, :primary => true
 
-desc "Reload Nginx"
-task :reload_nginx do 
-  sudo "/etc/init.d/nginx reload" 
-end
+#desc "Reload Nginx"
+#task :reload_nginx do 
+#  sudo "/etc/init.d/nginx reload" 
+#end
 
-after "deploy", "reload_nginx"
+#after "deploy", "reload_nginx"
 after "deploy", "deploy:cleanup"
