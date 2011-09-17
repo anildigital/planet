ebe73f6e72fc165c92e810bf194ade914e7ef2a0
Anil Wadghule <anildigital@gmail.com>
2011-09-17 22:57:25 +0530

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,4 +27,18 @@ role :db,  application, :primary => true
 #end
 
 #after "deploy", "reload_nginx"
+
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
+
 after "deploy", "deploy:cleanup"
